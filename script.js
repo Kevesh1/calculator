68e58e40eb6a2ac86c363a23b55f8b7f414e6ffb
Keivan <keivana@outlook.com>
2022-07-15 12:53:43 +0200

    operate works hardcoded

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const screen = document.querySelector('#screen');
 const buttons = document.querySelectorAll('.center');
+const operators = document.querySelectorAll('.right-side');
+let currentValue = 0;
 
 function add(a,b){
     return a + b;
@@ -34,6 +36,19 @@ buttons.forEach(button => {
     });
 });
 
+operators.forEach(operator => {
+    operator.addEventListener('click', e => {
+        if(currentValue !== 0){
+            screen.innerText = operate(e.target.id ,currentValue,parseInt(screen.innerText));
+            console.log(e.target.id);
+            console.log(screen.innerText);
+        }
+        currentValue = parseInt(screen.innerText);
+        screen.innerText = "";
+    });
+});
+
 function clearInput(){
     screen.innerText = "";
+    currentValue = 0;
 }
